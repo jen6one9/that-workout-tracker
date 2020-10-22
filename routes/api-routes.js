9db4e48f5238cf5fef8d2180eec9224b17e0f530
Jeneth Diesta <jen6one9@gmail.com>
2020-10-21 18:30:20 -0700

    updated server.js

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,2 +1,34 @@
-var routes = require ("express").Router() 
+var routes = require("express").Router()
 //link by looking at the front end java script file (start with api.js file because it has the routes)
+
+var db = require("../models/workoutmodels") //by having this line, Mongoose take care of creating the db and the collection!
+routes.get("/api/workouts", function (req, res) {
+    db.find()
+        .then(function (records) {
+            console.log("get route", records)
+            res.json(records)
+        })
+})
+routes.put("/api/workouts/:id", function (req, res) {
+    db.findByIdAndUpdate(req.params.id,{$push:{exercises:req.body}},{new:true})
+        .then(function (records) {
+            console.log("put route", records)
+            res.json(records)
+        })
+})
+routes.post("/api/workouts", function (req, res) {
+    db.create(req.body)
+        .then(function (records) {
+            console.log("post route", records)
+            res.json(records)
+        })
+})
+routes.get("/api/workouts", function (req, res) {
+    db.find({}).limit(15)
+        .then(function (records) {
+            console.log("get range route", records)
+            res.json(records)
+        })
+})
+
+module.exports = routes
--- server.js
+++ server.js
@@ -1,125 +1,30 @@
 
 const express = require("express");
-const mongojs = require("mongojs");
-const logger = require("morgan");
+const mongoose = require("mongoose");
+
 
 const app = express();
 
-app.use(logger("dev"));
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-const databaseUrl = "notetaker";
-const collections = ["notes"];
+mongoose.connect("mongodb://localhost//fitnesstracker",{
+    useNewUrlParser:true,
+    useFindAndModify:true
+})
 
-const db = mongojs(databaseUrl, collections);
-
-db.on("error", error => {
-  console.log("Database Error:", error);
-});
-
-app.get("/", (req, res) => {
-  res.send(index.html);
-});
-
-
-// 1. Add exercise to the database's collection
-// POST: /submit
-// ===========================================
-app.post("/submit", (req, res) => {
-  console.log(req.body);
-
-  db.notes.insert(req.body, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.send(data);
-    }
-  });
-});
-
-
-// 2. Retrieve all notes from the database's collection
-// GET: /all
-// ====================================================
-app.get("/all", (req, res) => {
-  db.notes.find({}, (error, data) => {
-    if (error) {
-      res.send(error);
-    } else {
-      res.json(data);
-    }
-  });
-});
-
-// 3. Retrieve one note in the database's collection by it's ObjectId
-// TIP: when searching by an id, the id needs to be passed in
-// as (mongojs.ObjectId(IdYouWantToFind))
-// GET: /find/:id
-// ==================================================================
-db.getCollection('system.notes').find({_id: ObjectId("enter hash id here")})
-
-
-app.get("/find/:id", (req, res) => {
-  db.notes.findOne(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
-
-// 4. Update one note in the database's collection by it's ObjectId
-// (remember, mongojs.ObjectId(IdYouWantToFind)
-// POST: /update/:id
-// ================================================================
-db.Collection.update({ObjectId:'enter hash id here'), {$note: enter_garbage}
-
-app.post("/update/:id", (req, res) => {
-  db.notes.update(
-    {
-      _id: mongojs.ObjectId(req.params.id)
-    },
-    {
-      $set: {
-        title: req.body.title,
-        note: req.body.note,
-        modified: Date.now()
-      }
-    },
-    (error, data) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.send(data);
-      }
-    }
-  );
-});
+var apiroutes = require("./routes/api-routes")
+var htmlroutes = require("./routes/html-routes")
+app.use(apiroutes)
+app.use(htmlroutes)
 
 
 
 
-
-
-
-// 5. Delete one note from the database's collection by it's ObjectId
-// (remember, mongojs.ObjectId(IdYouWantToFind)
-// DELETE: /delete/:id
-// ==================================================================
-db.Collection.remove({ObjectId:'enter hash id here') 
-// 6. Clear the entire note collection
-// DELETE: /clearall
-// ===================================
 
 // Listen on port 3000
 app.listen(3000, () => {
